89fd04ec405fede74b3ce0b5e55dd1dee5fc4663
bebopyunenvy <bebopyunenvy@gmail.com>
2025-10-22 10:56:31 +0800

    add components

---
+++ frontend/app/hotel-booking/selector/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export interface SelectorPageProps {}
+
+export default function SelectorPage({}: SelectorPageProps) {
+  return (
+    <>
+      <div>Selector Page</div>
+      <div>房間選擇</div>
+    </>
+  );
+}
